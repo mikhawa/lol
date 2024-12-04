474aa04c9bf01b2be01e121de6db3e0b4b71c96b
Baye Fall <magibsall@gmail.com>
2024-12-04 12:00:44 +0100

    sov5

--- src/Controller/EleveController.php
+++ src/Controller/EleveController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Repository\EleveRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class EleveController extends AbstractController
 {
-    #[Route('/eleve', name: 'app_eleve')]
-    public function index(): Response
+    #[Route('/eleve/{id}', name: 'eleve_show')]
+    public function show(int $id, EleveRepository $eleveRepository): Response
     {
+        $eleve = $eleveRepository->find($id);
+
+        if (!$eleve) {
+            throw $this->createNotFoundException('Élève non trouvé');
+        }
+
         return $this->render('eleve/index.html.twig', [
-            'controller_name' => 'EleveController',
+            'eleve' => $eleve, // Transmission de la variable au template
+        ]);
+    }
+    #[Route('/eleves', name: 'eleve_list')]
+    public function list(EleveRepository $eleveRepository): Response
+    {
+        $eleves = $eleveRepository->findAll();
+        return $this->render('eleve/list.html.twig', [
+            'eleves' => $eleves,
         ]);
     }
 }
