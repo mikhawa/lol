45ab37c1c03ae9a566a30516336ec8352c47e5a9
Baye Fall <magibsall@gmail.com>
2024-12-16 13:31:18 +0100

    Gérer insertion d'avatar

--- src/Controller/EleveController.php
+++ src/Controller/EleveController.php
@@ -11,6 +11,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class EleveController extends AbstractController
 {
@@ -34,26 +36,75 @@ class EleveController extends AbstractController
     }
 
     // Méthode pour ajouter un nouvel élève
+//    #[Route('/eleve/new', name: 'eleve_new', methods: ['GET', 'POST'])]
+//    public function new(Request $request, EntityManagerInterface $entityManager): Response
+//    {
+//        $eleve = new Eleve();
+//        $form = $this->createForm(EleveType::class, $eleve);
+//
+//        $form->handleRequest($request);
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $entityManager->persist($eleve);
+//            $entityManager->flush();
+//
+//            $this->addFlash('success', 'Élève ajouté avec succès.');
+//
+//            return $this->redirectToRoute('eleve_list');
+//        }
+//
+//        return $this->render('eleve/new.html.twig', [
+//            'form' => $form->createView(),
+//        ]);
+//    }
     #[Route('/eleve/new', name: 'eleve_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $eleve = new Eleve();
         $form = $this->createForm(EleveType::class, $eleve);
 
-        $form->handleRequest($request);
+        $form->handleRequest($request); // Gère la soumission du formulaire
+
         if ($form->isSubmitted() && $form->isValid()) {
+            // Gestion de l'upload de l'avatar
+            $avatarFile = $form->get('avatar')->getData(); // Récupérer le fichier uploadé
+
+            if ($avatarFile) {
+                // Générer un nom unique pour le fichier
+                $originalFilename = pathinfo($avatarFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename); // Assure un nom "safe"
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $avatarFile->guessExtension();
+
+                try {
+                    // Déplacer le fichier au répertoire upload configuré
+                    $avatarFile->move(
+                        $this->getParameter('avatars_directory'), // Défini dans `services.yaml`
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // Gérer les erreurs lors de l'upload
+                    $this->addFlash('error', 'Erreur lors de l\'upload de la photo.');
+                    return $this->redirectToRoute('eleve_new');
+                }
+
+                $eleve->setAvatar($newFilename); // Met à jour la propriété `avatar` de l'entité Eleve
+            }
+
+            // Persiste et sauvegarde en base de données
             $entityManager->persist($eleve);
             $entityManager->flush();
 
+            // Message flash de succès
             $this->addFlash('success', 'Élève ajouté avec succès.');
 
-            return $this->redirectToRoute('eleve_index');
+            // Redirige vers la liste
+            return $this->redirectToRoute('eleve_list');
         }
 
         return $this->render('eleve/new.html.twig', [
             'form' => $form->createView(),
         ]);
     }
+
 
     #[Route('/eleve/{id}/delete', name: 'eleve_delete', methods: ['POST'])]
     public function delete(Request $request, Eleve $eleve, EntityManagerInterface $entityManager): Response
