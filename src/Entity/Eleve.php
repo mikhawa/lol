eee9587e6591e4ca8f6615ca80bc2666b262f5f1
Baye Fall <magibsall@gmail.com>
2024-12-24 13:00:10 +0100

    reajustement breadcrumb-option_v3

--- src/Entity/Eleve.php
+++ src/Entity/Eleve.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EleveRepository::class)]
+#[ORM\Table(name: "eleve")]
 class Eleve
 {
     #[ORM\Id]
